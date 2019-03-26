2473454855c2522a4ee346c7a7a16006caece5bc
Rafael Ferreira <rafa0184@hotmail.com>
2019-03-26 19:01:02 -0300

    Layout Alterado

--- index.php
+++ index.php
@@ -1,24 +1,22 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" media="screen" href="style.css">
-    <script src="main.js"></script>
-</head>
-<body>
+<?php
+    require('templates/header.php');
+?>
     
     <div class="container-fluid box">
-            <div class="col-sm-12 topo">topo</div>
-            <div class="col-sm-2 menu sidebar">
-            <ul class="nav">
-    
-            </div>
-            <div class="col-sm-12 content">Conteudo</div>
-    </div>
+            <div class="col-sm-12 topo">
+                <h1>Sistema</h1>
+            </div><!--col-sm-12 topo-->
 
-</body>
-</html>
+            <div class="col-sm-2 menu sidebar">
+                <div class="menu-avatar">   
+                    <i class="fas fa-user"></i>
+                </div><!--menu-avatar-->
+            </div><!--col-sm-2 menu sidebar-->
+
+            <div class="col-sm-12 content">
+               
+            </div><!--col-sm-12 content-->
+    </div>
+<?php
+    require('templates/footer.php');
+?>

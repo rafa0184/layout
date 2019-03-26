8e376f8fa4869f1b7aae961fa9c74a16f5754e17
rafa0184 <rafa0184@hotmail.com>
2019-03-25 21:23:30 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css">
+    <script src="main.js"></script>
+</head>
+<body>
+    
+    <div class="container-fluid box">
+            <div class="col-sm-12 topo">topo</div>
+            <div class="col-sm-2 menu sidebar">
+            <ul class="nav">
+    
+            </div>
+            <div class="col-sm-12 content">Conteudo</div>
+    </div>
+
+</body>
+</html>

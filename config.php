7cc335820e96e6b2f4b1ac7966a5b19628863e84
rafa0184 <rafa0184@hotmail.com>
2019-04-07 18:29:38 -0300

    Add files via upload

--- config.php
+++ config.php
@@ -1,23 +1,5 @@
 <?php
-    //Constantes para configuração de banco de dados
-    //define('HOST',  'localhost');
-    //define('DB',    'grupozelo');
-    //define('USER',  'rafael');
-    //define('PASS',  'iTahk7ahni');
 
-    define('INCLUDE_PATH','http://localhost/layout/');
-    define('CLASSES','http://localhost/layout/Classes/');
+try{
 
-    
-    //Carregamento automatico de classes
-   // $autoload = function($class){
-   //     if(file_exists('http://localhost/layout/Classes/' .$class.'.php')){
-   //         include('http://localhost/layout/Classes/' .$class.'.php');
-   //     } else {
-   //         print $class;
-   //         echo "Classe não encontradas";
-   //     }
-   // };
-
-    //spl_autoload_register($autoload);
-?>
+} catch()
